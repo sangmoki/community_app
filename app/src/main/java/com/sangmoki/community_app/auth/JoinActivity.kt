d4682b3a7c554f56806af40b6915bfba3fdb3e54
이상목 <yoochang4176@naver.com>
2024-06-21 17:52:23 +0900

    Feat: "firebase 회원가입 이벤트 로직 구현"

--- app/src/main/java/com/sangmoki/community_app/auth/JoinActivity.kt
+++ app/src/main/java/com/sangmoki/community_app/auth/JoinActivity.kt
@@ -1,13 +1,20 @@
 package com.sangmoki.community_app.auth
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 import com.sangmoki.community_app.R
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +23,39 @@ class JoinActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // firebase 인증 객체 생성
+        val currentUser = auth.currentUser
+
+        // 회원가입 버튼 객체 생성
+        val joinBtn = findViewById<TextView>(R.id.joinBtn)
+
+        // 회원가입 버튼 클릭 이벤트
+        joinBtn.setOnClickListener {
+            
+            val email = findViewById<TextView>(R.id.emailText).text.toString()
+            val password = findViewById<TextView>(R.id.passwordText).text.toString()
+            
+            // firebase 회원가입 시도 이벤트
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d("회원가입 시도 =======>", "회원가입 성공 !")
+                        Toast.makeText(
+                            baseContext,
+                            "회원가입 성공 !",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    } else {
+                        Log.w("회원가입 시도 =======>", "회원가입 실패 !")
+                        Toast.makeText(
+                            baseContext,
+                            "회원가입 실패 !",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
         }
     }
 }
