a0e3639558a2f9411b0d2085afbef121c91be0ea
이상목 <yoochang4176@naver.com>
2024-06-23 19:12:26 +0900

    Feat: "로그아웃 기능 로직 구현"

--- app/src/main/java/com/sangmoki/community_app/MainActivity.kt
+++ app/src/main/java/com/sangmoki/community_app/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.sangmoki.community_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.sangmoki.community_app.auth.IntroActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,20 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+
+        // 로그아웃 버튼 클릭 이벤트
+        findViewById<Button>(R.id.logoutBtn).setOnClickListener {
+            // 로그아웃 이벤트
+            auth.signOut()
+
+            // 스택 제거 후 인트로 화면으로 이동
+            val intent = Intent(this, IntroActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
         }
     }
 }
