77894b6fa57a20e7b7eaa29266de359f520c411f
이상목 <yoochang4176@naver.com>
2024-06-23 19:15:23 +0900

    Feat: "익명 로그인 기능 로직 구현"

--- app/src/main/java/com/sangmoki/community_app/auth/IntroActivity.kt
+++ app/src/main/java/com/sangmoki/community_app/auth/IntroActivity.kt
@@ -2,10 +2,16 @@ package com.sangmoki.community_app.auth
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.sangmoki.community_app.MainActivity
 import com.sangmoki.community_app.R
 import com.sangmoki.community_app.databinding.ActivityIntroBinding
@@ -14,10 +20,16 @@ class IntroActivity : AppCompatActivity() {
 
     // 바인딩 라이브러리 객체 생성
     private lateinit var binding: ActivityIntroBinding
+    // firebase 인증 객체 생성
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // firebase 인증 객체 생성
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
         
         // Intro 레이아웃 바인딩
         binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
@@ -36,8 +48,25 @@ class IntroActivity : AppCompatActivity() {
         
         // 비회원 로그인 버튼 클릭 이벤트
         binding.guestActivityBtn.setOnClickListener {
-            var intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            // 익명 로그인 시도
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d("익명 로그인 시도 ======>>", "익명 로그인 성공 !")
+                        Toast.makeText(
+                            baseContext,
+                            "익명 로그인 성공 !",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    } else {
+                        Log.d("익명 로그인 시도 ======>>", "익명 로그인 실패 !")
+                        Toast.makeText(
+                            baseContext,
+                            "익명 로그인 실패 !",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
         }
         
     }
