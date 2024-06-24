b2c91087e4f515e375fc32a113196f5ea0c398d2
이상목 <yoochang4176@naver.com>
2024-06-24 12:03:51 +0900

    Feat: "firebase auth uid 값으로 페이지 이동 분기처리"

--- app/src/main/java/com/sangmoki/community_app/MainActivity.kt
+++ app/src/main/java/com/sangmoki/community_app/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.auth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.sangmoki.community_app.auth.IntroActivity
@@ -30,14 +29,14 @@ class MainActivity : AppCompatActivity() {
         val currentUser = auth.currentUser
 
         // 로그아웃 버튼 클릭 이벤트
-//        findViewById<Button>(R.id.logoutBtn).setOnClickListener {
-//            // 로그아웃 이벤트
-//            auth.signOut()
-//
-//            // 스택 제거 후 인트로 화면으로 이동
-//            val intent = Intent(this, IntroActivity::class.java)
-//            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-//            startActivity(intent)
-//        }
+        findViewById<Button>(R.id.logoutBtn).setOnClickListener {
+            // 로그아웃 이벤트
+            auth.signOut()
+
+            // 스택 제거 후 인트로 화면으로 이동
+            val intent = Intent(this, IntroActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/sangmoki/community_app/SplashActivity.kt
+++ app/src/main/java/com/sangmoki/community_app/SplashActivity.kt
@@ -3,13 +3,20 @@ package com.sangmoki.community_app
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.ktx.auth
 import com.sangmoki.community_app.auth.IntroActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    // Firebase 인증 객체
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,10 +27,24 @@ class SplashActivity : AppCompatActivity() {
             insets
         }
 
-        // 3초 후 IntroActivity로 이동
-        Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        }, 3000)
+        // Firebase 인증 객체 초기화
+        auth = Firebase.auth
+
+        // 로그인이 되어있다면, MainActivity로 이동 아니라면 IntroActivity로 이동
+        if (auth.currentUser?.uid == null) {
+            // 3초 후 IntroActivity로 이동
+            Handler().postDelayed({
+                startActivity(Intent(this, IntroActivity::class.java))
+                finish()
+            }, 3000)
+        } else {
+            // 3초 후 IntroActivity로 이동
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 3000)
+        }
+
+
     }
 }
