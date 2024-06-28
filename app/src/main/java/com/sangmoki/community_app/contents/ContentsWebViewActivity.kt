87d9d3fae3257f39b8eb55e7c34e9d1236c2075d
이상목 <yoochang4176@naver.com>
2024-06-28 14:04:21 +0900

    Feat: "웹뷰 띄워줄 액티비티 생성"

---
+++ app/src/main/java/com/sangmoki/community_app/contents/ContentsWebViewActivity.kt
@@ -0,0 +1,21 @@
+package com.sangmoki.community_app.contents
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sangmoki.community_app.R
+
+class ContentsWebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_contents_web_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
