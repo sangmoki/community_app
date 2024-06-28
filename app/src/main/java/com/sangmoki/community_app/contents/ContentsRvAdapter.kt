cbbc39d17d0ac1e677089d9370b0b98579253868
이상목 <yoochang4176@naver.com>
2024-06-28 13:52:15 +0900

    Feat: "글라이드 라이브러리 url 연결"

--- app/src/main/java/com/sangmoki/community_app/contents/ContentsRvAdapter.kt
+++ app/src/main/java/com/sangmoki/community_app/contents/ContentsRvAdapter.kt
@@ -1,13 +1,16 @@
 package com.sangmoki.community_app.contents
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.sangmoki.community_app.R
 
-class ContentsRvAdapter(val items: ArrayList<ContentsModel>) : RecyclerView.Adapter<ContentsRvAdapter.ViewHolder>() {
+class ContentsRvAdapter(val context: Context, val items: ArrayList<ContentsModel>) : RecyclerView.Adapter<ContentsRvAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentsRvAdapter.ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.contents_rv_item, parent, false)
         return ViewHolder(v)
@@ -24,7 +27,17 @@ class ContentsRvAdapter(val items: ArrayList<ContentsModel>) : RecyclerView.Adap
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bindItems(item: ContentsModel) {
             val title = itemView.findViewById<TextView>(R.id.title)
+            val imgUrl = itemView.findViewById<ImageView>(R.id.imageUrl)
+
+            // title에 item의 title 맵핑해준다.
             title.text = item.title
+
+            // 글라이드 라이브러리를 통해
+            Glide.with(context)
+                // item의 imgUrl에
+                .load(item.imgUrl)
+                // imgUrl에 이미지를 넣어준다.
+                .into(imgUrl)
 
         }
     }
