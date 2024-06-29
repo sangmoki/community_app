dc095f795e7f5b929a83019f2447ec4fff3d81ff
이상목 <yoochang4176@naver.com>
2024-06-29 17:26:33 +0900

    Feat: "범용적으로 firebase auth를 사용하기 위한 클래스 생성 및 로직 구현"

---
+++ app/src/main/java/com/sangmoki/community_app/util/FirebaseAuth.kt
@@ -0,0 +1,17 @@
+package com.sangmoki.community_app.util
+
+import com.google.firebase.auth.FirebaseAuth
+
+// 범용적으로 사용하기 위해 FirebaseAuth 클래스 생성
+class FirebaseAuth {
+    companion object {
+        // Firebase 인증 객체
+        private lateinit var auth: FirebaseAuth
+
+        // 현재 로그인 유저의 uid 값 반환
+        fun getUid(): String {
+            auth = FirebaseAuth.getInstance()
+            return auth.currentUser?.uid.toString()
+        }
+    }
+}
