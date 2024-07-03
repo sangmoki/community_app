65bbbc57b13fce6c2b5c93191e321c828f4f066d
이상목 <yoochang4176@naver.com>
2024-07-03 16:24:10 +0900

    Feat: "생성한 ListView 및 Adapter talk 프래그먼트에 연결"

--- app/src/main/java/com/sangmoki/community_app/adapter/BoardLvAdapter.kt
+++ app/src/main/java/com/sangmoki/community_app/adapter/BoardLvAdapter.kt
@@ -1,4 +1,32 @@
 package com.sangmoki.community_app.adapter
 
-class BoardLvAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.sangmoki.community_app.R
+import com.sangmoki.community_app.model.BoardModel
+
+class BoardLvAdapter(val boardList: MutableList<BoardModel>) : BaseAdapter() {
+    override fun getCount(): Int {
+        return boardList.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return boardList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+        var convertView = convertView
+        if (convertView == null) {
+               convertView = LayoutInflater.from(parent?.context).inflate(R.layout.board_list_item, parent, false)
+        }
+
+        return convertView!!
+    }
 }
--- app/src/main/java/com/sangmoki/community_app/fragments/TalkFragment.kt
+++ app/src/main/java/com/sangmoki/community_app/fragments/TalkFragment.kt
@@ -9,8 +9,10 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.sangmoki.community_app.R
+import com.sangmoki.community_app.adapter.BoardLvAdapter
 import com.sangmoki.community_app.board.BoardWriteActivity
 import com.sangmoki.community_app.databinding.FragmentTalkBinding
+import com.sangmoki.community_app.model.BoardModel
 
 class TalkFragment : Fragment() {
 
@@ -28,6 +30,12 @@ class TalkFragment : Fragment() {
 
         // DataBinding 설정
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_talk, container, false)
+
+        val boardList = mutableListOf<BoardModel>()
+        boardList.add(BoardModel("제목1", "내용1", "작성자1", "2021-09-01"))
+
+        val adapter = BoardLvAdapter(boardList)
+        binding.boardListView.adapter = adapter
 
         binding.writeBtn.setOnClickListener {
             val intent = Intent(context, BoardWriteActivity::class.java)
